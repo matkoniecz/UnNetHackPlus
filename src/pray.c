17bc7879aedd109f6e64d537bfbdbe9faae81419
Bulwersator <bulwersator@onet.pl>
2013-01-01 19:31:43 +0100

    YAFM for ascension during hallucination
    author:  Adam Borowski (part of Assorted YAFMs 1.0 -
    http://bilious.alt.org/?121 )

--- src/pray.c
+++ src/pray.c
@@ -1325,7 +1325,8 @@ dosacrifice()
 #endif
 #endif
 
-pline("An invisible choir sings, and you are bathed in radiance...");
+		pline("%s sings, and you are bathed in radiance...",
+			Hallucination?"The fat lady":"An invisible choir");
 		godvoice(altaralign, "Congratulations, mortal!");
 		display_nhwindow(WIN_MESSAGE, FALSE);
 verbalize("In return for thy service, I grant thee the gift of Immortality!");
