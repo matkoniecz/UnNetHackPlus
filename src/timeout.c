4f77b116edb12236e46751e849e8dca409a4a117
Bulwersator <bulwersator@gmail.com>
2013-03-11 08:21:01 +0100

    saner luck expiration
    luck expires faster with the amulet/angered god only for positive luck values (yes, without this change angering god causes negative luck to expire faster)

--- src/timeout.c
+++ src/timeout.c
@@ -174,7 +174,7 @@ nh_timeout()
 		baseluck -= 1;
 	}
 
-	if (u.uluck != baseluck && moves % (u.uhave.amulet || u.ugangr ? 300 : 600) == 0) {
+	if (u.uluck != baseluck && moves % (((u.uhave.amulet || u.ugangr) && u.uluck > 0) ? 300 : 600) == 0) {
 	/* Cursed luckstones stop bad luck from timing out; blessed luckstones
 	 * stop good luck from timing out; normal luckstones stop both;
 	 * neither is stopped if you don't have a luckstone.
