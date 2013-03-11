06377f88cc1cfaaf8b82b6a4e5c9188826e0b43f
Bulwersator <bulwersator@gmail.com>
2013-03-11 08:08:11 +0100

    refactor eataccessory

--- src/eat.c
+++ src/eat.c
@@ -1721,139 +1721,147 @@ struct obj *otmp;
 	/* (How often do you even _find_ 3 rings of polymorph in a game?) */
 	oldprop = u.uprops[objects[typ].oc_oprop].intrinsic;
 	if (otmp == uleft || otmp == uright) {
-	    Ring_gone(otmp);
-	    if (u.uhp <= 0) return; /* died from sink fall */
+		Ring_gone(otmp);
+		if (u.uhp <= 0) {
+			return; /* died from sink fall */
+		}
 	}
 	otmp->known = otmp->dknown = 1; /* by taste */
 	if (!rn2(otmp->oclass == RING_CLASS ? 3 : 5)) {
-	  switch (otmp->otyp) {
-	    default:
-	        if (!objects[typ].oc_oprop) break; /* should never happen */
-
-		if (!(u.uprops[objects[typ].oc_oprop].intrinsic & FROMOUTSIDE))
-		    accessory_has_effect(otmp);
-
-		u.uprops[objects[typ].oc_oprop].intrinsic |= FROMOUTSIDE;
-
 		switch (typ) {
-		  case RIN_SEE_INVISIBLE:
-		    set_mimic_blocking();
-		    see_monsters();
-		    if (Invis && !oldprop && !ESee_invisible &&
-				!perceives(youmonst.data) && !Blind) {
-			newsym(u.ux,u.uy);
-			pline("Suddenly you can see yourself.");
+		default:
+			if (!objects[typ].oc_oprop) {
+				warning("impossible happened in function eataccessory");
+				break; /* should never happen */
+			}
+			if (!(u.uprops[objects[typ].oc_oprop].intrinsic & FROMOUTSIDE)) {
+				accessory_has_effect(otmp);
+			}
+			u.uprops[objects[typ].oc_oprop].intrinsic |= FROMOUTSIDE;
+			switch (typ) {
+			case RIN_SEE_INVISIBLE:
+				set_mimic_blocking();
+				see_monsters();
+				if (Invis && !oldprop && !ESee_invisible && !perceives(youmonst.data) && !Blind) {
+					newsym(u.ux,u.uy);
+					pline("Suddenly you can see yourself.");
+					makeknown(typ);
+				}
+				break;
+			case RIN_INVISIBILITY:
+				if (!oldprop && !EInvis && !BInvis && !See_invisible && !Blind) {
+					newsym(u.ux,u.uy);
+					Your("body takes on a %s transparency...", Hallucination ? "normal" : "strange");
+					makeknown(typ);
+				}
+				break;
+			case RIN_PROTECTION_FROM_SHAPE_CHAN:
+				rescham();
+				break;
+			case RIN_LEVITATION:
+				/* undo the `.intrinsic |= FROMOUTSIDE' done above */
+				u.uprops[LEVITATION].intrinsic = oldprop;
+				if (!Levitation) {
+					float_up();
+					incr_itimeout(&HLevitation, d(10,20));
+					makeknown(typ);
+				}
+				break;
+			}
+			break;
+		case RIN_ADORNMENT:
+			accessory_has_effect(otmp);
+			if (adjattrib(A_CHA, otmp->spe, -1)) {
+				makeknown(typ);
+			}
+			break;
+		case RIN_GAIN_STRENGTH:
+			accessory_has_effect(otmp);
+			if (adjattrib(A_STR, otmp->spe, -1)) {
+				makeknown(typ);
+			}
+			break;
+		case RIN_GAIN_CONSTITUTION:
+			accessory_has_effect(otmp);
+			if (adjattrib(A_CON, otmp->spe, -1)) {
+				makeknown(typ);
+			}
+			break;
+		case RIN_GAIN_INTELLIGENCE:
+			accessory_has_effect(otmp);
+			if (adjattrib(A_INT, otmp->spe, -1)) {
+				makeknown(typ);
+			}
+			break;
+		case RIN_GAIN_WISDOM:
+			accessory_has_effect(otmp);
+			if (adjattrib(A_WIS, otmp->spe, -1)) {
+				makeknown(typ);
+			}
+			break;
+		case RIN_GAIN_DEXTERITY:
+			accessory_has_effect(otmp);
+			if (adjattrib(A_DEX, otmp->spe, -1)) {
+				makeknown(typ);
+			}
+			break;
+		case RIN_INCREASE_ACCURACY:
+			accessory_has_effect(otmp);
+			u.uhitinc += otmp->spe;
+			break;
+		case RIN_INCREASE_DAMAGE:
+			accessory_has_effect(otmp);
+			u.udaminc += otmp->spe;
+			break;
+		case RIN_PROTECTION:
+			accessory_has_effect(otmp);
+			HProtection |= FROMOUTSIDE;
+			u.ublessed += otmp->spe;
+			flags.botl = 1;
+			break;
+		case RIN_FREE_ACTION:
+			/* Give sleep resistance instead */
+			if (!(HSleep_resistance & FROMOUTSIDE)) {
+				accessory_has_effect(otmp);
+			}
+			if (!Sleep_resistance) {
+				You_feel("wide awake.");
+			}
+			HSleep_resistance |= FROMOUTSIDE;
+			break;
+		case AMULET_OF_CHANGE:
+			accessory_has_effect(otmp);
 			makeknown(typ);
-		    }
-		    break;
-		  case RIN_INVISIBILITY:
-		    if (!oldprop && !EInvis && !BInvis &&
-					!See_invisible && !Blind) {
-			newsym(u.ux,u.uy);
-			Your("body takes on a %s transparency...",
-				Hallucination ? "normal" : "strange");
+			change_sex();
+			You("are suddenly very %s!", flags.female ? "feminine" : "masculine");
+			flags.botl = 1;
+			break;
+		case AMULET_OF_UNCHANGING:
+			/* un-change: it's a pun */
+			if (!Unchanging && Upolyd) {
+				accessory_has_effect(otmp);
 			makeknown(typ);
-		    }
-		    break;
-		  case RIN_PROTECTION_FROM_SHAPE_CHAN:
-		    rescham();
-		    break;
-		  case RIN_LEVITATION:
-		    /* undo the `.intrinsic |= FROMOUTSIDE' done above */
-		    u.uprops[LEVITATION].intrinsic = oldprop;
-		    if (!Levitation) {
-			float_up();
-			incr_itimeout(&HLevitation, d(10,20));
-			makeknown(typ);
-		    }
-		    break;
+			rehumanize();
+			}
+			break;
+		case AMULET_OF_STRANGULATION: /* bad idea! */
+			/* no message--this gives no permanent effect */
+			choke(otmp);
+			break;
+		case AMULET_OF_RESTFUL_SLEEP: /* another bad idea! */
+			if (!(HSleeping & FROMOUTSIDE)) {
+				accessory_has_effect(otmp);
+			}
+			HSleeping = FROMOUTSIDE | rnd(100);
+			break;
+		case RIN_SUSTAIN_ABILITY:
+		case AMULET_OF_LIFE_SAVING:
+		case AMULET_OF_REFLECTION: /* nice try */
+		/* can't eat Amulet of Yendor or fakes,
+		 * and no oc_prop even if you could -3.
+		 */
+			break;
 		}
-		break;
-	    case RIN_ADORNMENT:
-		accessory_has_effect(otmp);
-		if (adjattrib(A_CHA, otmp->spe, -1))
-		    makeknown(typ);
-		break;
-	    case RIN_GAIN_STRENGTH:
-		accessory_has_effect(otmp);
-		if (adjattrib(A_STR, otmp->spe, -1))
-		    makeknown(typ);
-		break;
-	    case RIN_GAIN_CONSTITUTION:
-		accessory_has_effect(otmp);
-		if (adjattrib(A_CON, otmp->spe, -1))
-		    makeknown(typ);
-		break;
-	    case RIN_GAIN_INTELLIGENCE:
-		accessory_has_effect(otmp);
-		if (adjattrib(A_INT, otmp->spe, -1))
-		    makeknown(typ);
-		break;
-	    case RIN_GAIN_WISDOM:
-		accessory_has_effect(otmp);
-		if (adjattrib(A_WIS, otmp->spe, -1))
-		    makeknown(typ);
-		break;
-	    case RIN_GAIN_DEXTERITY:
-		accessory_has_effect(otmp);
-		if (adjattrib(A_DEX, otmp->spe, -1))
-		    makeknown(typ);
-		break;
-	    case RIN_INCREASE_ACCURACY:
-		accessory_has_effect(otmp);
-		u.uhitinc += otmp->spe;
-		break;
-	    case RIN_INCREASE_DAMAGE:
-		accessory_has_effect(otmp);
-		u.udaminc += otmp->spe;
-		break;
-	    case RIN_PROTECTION:
-		accessory_has_effect(otmp);
-		HProtection |= FROMOUTSIDE;
-		u.ublessed += otmp->spe;
-		flags.botl = 1;
-		break;
-	    case RIN_FREE_ACTION:
-		/* Give sleep resistance instead */
-		if (!(HSleep_resistance & FROMOUTSIDE))
-		    accessory_has_effect(otmp);
-		if (!Sleep_resistance)
-		    You_feel("wide awake.");
-		HSleep_resistance |= FROMOUTSIDE;
-		break;
-	    case AMULET_OF_CHANGE:
-		accessory_has_effect(otmp);
-		makeknown(typ);
-		change_sex();
-		You("are suddenly very %s!",
-		    flags.female ? "feminine" : "masculine");
-		flags.botl = 1;
-		break;
-	    case AMULET_OF_UNCHANGING:
-		/* un-change: it's a pun */
-		if (!Unchanging && Upolyd) {
-		    accessory_has_effect(otmp);
-		    makeknown(typ);
-		    rehumanize();
-		}
-		break;
-	    case AMULET_OF_STRANGULATION: /* bad idea! */
-		/* no message--this gives no permanent effect */
-		choke(otmp);
-		break;
-	    case AMULET_OF_RESTFUL_SLEEP: /* another bad idea! */
-		if (!(HSleeping & FROMOUTSIDE))
-		    accessory_has_effect(otmp);
-		HSleeping = FROMOUTSIDE | rnd(100);
-		break;
-	    case RIN_SUSTAIN_ABILITY:
-	    case AMULET_OF_LIFE_SAVING:
-	    case AMULET_OF_REFLECTION: /* nice try */
-	    /* can't eat Amulet of Yendor or fakes,
-	     * and no oc_prop even if you could -3.
-	     */
-		break;
-	  }
 	}
 }
 
