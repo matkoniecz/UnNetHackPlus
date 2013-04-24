matkoniecz/UnNetHackPlus

commit 677fb3a3e853c6543a81841bacb8dffce274f843

unconditional enabling wizmode + tweaks, refactoring

src/mklev.c
@@ -6,11 +6,7 @@
 /* #define DEBUG */	/* uncomment to enable code debugging */
 
 #ifdef DEBUG
-# ifdef WIZARD
 #define debugpline	if (wizard) pline
-# else
-#define debugpline	pline
-# endif
 #endif
 
 /* for UNIX, Rand #def'd to (long)lrand48() or (long)random() */
@@ -890,13 +886,12 @@ makelevel()
 	do {
 	    switch (rn2(15)) {
 	    default:
-#ifdef WIZARD
-	if(wizard && nh_getenv("SHOPTYPE")) mkroom(SHOPBASE); else
-#endif
-	if (u_depth > 1 &&
-	    u_depth < depth(&medusa_level) &&
-	    nroom >= room_threshold &&
-	    rn2(u_depth) < 3) { mkroom(SHOPBASE); break; }
+		if(wizard && nh_getenv("SHOPTYPE")) {
+			mkroom(SHOPBASE);
+		} else if (u_depth > 1 && u_depth < depth(&medusa_level) && nroom >= room_threshold && rn2(u_depth) < 3) {
+			mkroom(SHOPBASE); 
+			break; 
+		}
 	    case 0:
 		if (u_depth > 4 && !rn2(6)) { mkroom(COURT); break; }
 	    case 1:
@@ -1828,11 +1823,9 @@ xchar x, y;
 	}
 
 	/* Already set or 2/3 chance of deferring until a later level. */
-	if (source->dnum < n_dgns || (rn2(3)
-#ifdef WIZARD
-				      && !wizard
-#endif
-				      )) return;
+	if (source->dnum < n_dgns || (rn2(3))) {
+		return;
+	}
 
 	if (! (u.uz.dnum == oracle_level.dnum	    /* in main dungeon */
 		&& !at_dgn_entrance("The Quest")    /* but not Quest's entry */

configure
@@ -634,7 +634,6 @@ FILE_AREA_VAR
 FILE_AREA_DOC
 GROUP
 OWNER
-WIZARD
 WHEREIS_FILE
 DUMP_HTML_CSS_FILE
 DUMPMSGS
@@ -786,7 +785,6 @@ enable_dump_html_css_file
 enable_dump_html_css_embedded
 enable_utf8_glyphs
 enable_whereis_file
-enable_wizmode
 with_owner
 with_group
 enable_file_areas
@@ -1496,8 +1494,6 @@ Optional Features:
   --enable-whereis-file=FILE
                           Log information about running games (default=no,
                           default file=[%n.whereis])
-  --enable-wizmode=USER   enable wizard mode for the specified user
-                          (default=yes, default user=[wizard])
   --enable-file-areas     Place files in different directories (default=yes)
 
 Optional Packages:
@@ -7012,34 +7008,6 @@ cat >>confdefs.h <<_ACEOF
 _ACEOF
 
       WHEREIS_FILE="$whereis_file"
-
-   else
-      { $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5
-$as_echo "no" >&6; }
-   fi
-
-
-{ $as_echo "$as_me:${as_lineno-$LINENO}: checking whether to enable wizmode" >&5
-$as_echo_n "checking whether to enable wizmode... " >&6; }
-   # Check whether --enable-wizmode was given.
-if test "${enable_wizmode+set}" = set; then :
-  enableval=$enable_wizmode; if test "$enableval" != no; then enable_wizmode=yes;
-         if test "$enableval" != yes; then wizmode=$enableval;
-         else wizmode=wizard; fi;
-        else enable_wizmode=no; fi
-else
-  enable_wizmode=yes;wizmode=wizard
-fi
-
-   if test "$enable_wizmode" != "no" ; then
-      { $as_echo "$as_me:${as_lineno-$LINENO}: result: yes (user $wizmode)" >&5
-$as_echo "yes (user $wizmode)" >&6; }
-
-cat >>confdefs.h <<_ACEOF
-#define WIZARD                "$wizmode"
-_ACEOF
-
-      WIZARD="$wizmode"
 
    else
       { $as_echo "$as_me:${as_lineno-$LINENO}: result: no" >&5

include/config.h
@@ -145,15 +145,10 @@
 
 /*
  * Section 2:	Some global parameters and filenames.
- *		Commenting out WIZARD, LOGFILE, NEWS or PANICLOG removes that
- *		feature from the game; otherwise set the appropriate wizard
- *		name.  LOGFILE, NEWS and PANICLOG refer to files in the
- *		playground.
+ *		Commenting out LOGFILE, NEWS or PANICLOG removes that
+ *		feature from the game LOGFILE, NEWS and PANICLOG refer to 
+ *		files in the playground.
  */
-
-#ifndef WIZARD		/* allow for compile-time or Makefile changes */
-# define WIZARD  "wizard" /* the person allowed to use the -D option */
-#endif
 
 #define LOGFILE "logfile"	/* larger file for debugging purposes */
 #define LOGAREA FILE_AREA_VAR
@@ -453,6 +448,18 @@ typedef long glyph_t;
 #define REINCARNATION	/* Special Rogue-like levels */
 
 /* End of Section 7 */
+
+/*
+ * Section 8:  FICTIONAL DEFINES THAT MUST BE DEFINED
+ *
+ * Commenting these defines is not allowes, as #ifdef etc were removed from code 
+ * and features are incorporated into UnNetHackPlus without evil preprocessor instructions littering code
+ * these defines are also listed in build_pseudo_opts (util\makedefs.c)
+ * but these constants must be set to some value
+ */
+#define WIZARD  "wizard" /* the person allowed to use the -D option */
+
+/* End of Section 8 */
 
 #include "global.h"	/* Define everything else according to choices above */
 

include/extern.h
@@ -154,7 +154,6 @@ E int NDECL(doprev_message);
 E int NDECL(timed_occupation);
 E int NDECL(wiz_attributes);
 E int NDECL(enter_explore_mode);
-# ifdef WIZARD
 E int NDECL(wiz_detect);
 E int NDECL(wiz_genesis);
 E int NDECL(wiz_identify);
@@ -162,7 +161,6 @@ E int NDECL(wiz_level_tele);
 E int NDECL(wiz_map);
 E int NDECL(wiz_where);
 E int NDECL(wiz_wish);
-# endif /* WIZARD */
 #endif /* USE_TRAMPOLI */
 E void NDECL(reset_occupations);
 E void FDECL(set_occupation, (int (*)(void),const char *,int));
@@ -171,9 +169,7 @@ E char NDECL(pgetchar);
 E void FDECL(pushch, (CHAR_P));
 E void FDECL(savech, (CHAR_P));
 #endif
-#ifdef WIZARD
 E void NDECL(add_debug_extended_commands);
-#endif /* WIZARD */
 E void FDECL(rhack, (char *));
 E int NDECL(doextlist);
 E int NDECL(extcmd_via_menu);
@@ -188,9 +184,7 @@ E int FDECL(isok, (int,int));
 E int FDECL(get_adjacent_loc, (const char *, const char *, XCHAR_P, XCHAR_P, coord *));
 E const char *FDECL(click_to_cmd, (int,int,int));
 E char NDECL(readchar);
-#ifdef WIZARD
 E void NDECL(sanity_check);
-#endif
 E char FDECL(yn_function, (const char *, const char *, CHAR_P));
 E char FDECL(paranoid_yn, (const char *, BOOLEAN_P));
 
@@ -555,9 +549,7 @@ E int FDECL(induced_align, (int));
 E boolean FDECL(Invocation_lev, (d_level *));
 E xchar NDECL(level_difficulty);
 E schar FDECL(lev_by_name, (const char *));
-#ifdef WIZARD
 E schar FDECL(print_dungeon, (BOOLEAN_P,schar *,xchar *));
-#endif
 E int NDECL(donamelevel);
 E int NDECL(dooverview);
 E void FDECL(forget_mapseen, (int));
@@ -695,7 +687,7 @@ E void NDECL(set_savefile_name);
 #ifdef INSURANCE
 E void FDECL(save_savefile_name, (int));
 #endif
-#if defined(WIZARD) && !defined(MICRO)
+#if !defined(MICRO)
 E void NDECL(set_error_savefile);
 #endif
 E int NDECL(create_savefile);
@@ -715,9 +707,7 @@ E boolean FDECL(can_read_file, (const char *));
 #endif
 E void FDECL(read_config_file, (const char *));
 E void FDECL(check_recordfile, (const char *));
-#if defined(WIZARD)
 E void NDECL(read_wizkit);
-#endif
 E void FDECL(paniclog, (const char *, const char *));
 E int FDECL(validate_prefix_locations, (char *));
 E char** NDECL(get_saved_games);
@@ -947,9 +937,7 @@ E boolean FDECL(obj_is_burning, (struct obj *));
 E void FDECL(obj_split_light_source, (struct obj *, struct obj *));
 E void FDECL(obj_merge_light_sources, (struct obj *,struct obj *));
 E int FDECL(candle_light_range, (struct obj *));
-#ifdef WIZARD
 E int NDECL(wiz_light_sources);
-#endif
 
 /* ### lock.c ### */
 
@@ -1183,9 +1171,7 @@ E void FDECL(add_to_buried, (struct obj *));
 E void FDECL(dealloc_obj, (struct obj *));
 E void FDECL(obj_ice_effects, (int, int, BOOLEAN_P));
 E long FDECL(peek_at_iced_corpse_age, (struct obj *));
-#ifdef WIZARD
 E void NDECL(obj_sanity_check);
-#endif
 
 /* ### mkroom.c ### */
 
@@ -1795,9 +1781,7 @@ E void FDECL(do_genocide, (int,BOOLEAN_P));
 E void FDECL(punish, (struct obj *));
 E void NDECL(unpunish);
 E boolean FDECL(cant_create, (int *, BOOLEAN_P));
-#ifdef WIZARD
 E boolean NDECL(create_particular);
-#endif
 E void FDECL(drop_boulder_on_player, (BOOLEAN_P, BOOLEAN_P, BOOLEAN_P, BOOLEAN_P));
 E int  FDECL(drop_boulder_on_monster, (int, int, BOOLEAN_P, BOOLEAN_P));
 
@@ -2144,10 +2128,8 @@ E boolean FDECL(obj_is_local, (struct obj *));
 E void FDECL(save_timers, (int,int,int));
 E void FDECL(restore_timers, (int,int,BOOLEAN_P,long));
 E void FDECL(relink_timers, (BOOLEAN_P));
-#ifdef WIZARD
 E int NDECL(wiz_timeout_queue);
 E void NDECL(timer_sanity_check);
-#endif
 
 /* ### topten.c ### */
 

include/flag.h
@@ -214,10 +214,8 @@ struct instance_flags {
 #ifdef SIMPLE_MAIL
 	boolean simplemail;	/* simple mail format $NAME:$MESSAGE */
 #endif
-#ifdef WIZARD
 	boolean  sanity_check;	/* run sanity checks */
 	boolean  mon_polycontrol;	/* debug: control monster polymorphs */
-#endif
 #ifdef TTY_GRAPHICS
 	char prevmsg_window;	/* type of old message window to use */
 #endif

include/trampoli.h
@@ -26,7 +26,6 @@
 #define doprev_message() doprev_message_()
 #define timed_occupation() timed_occupation_()
 #define wiz_attributes()   wiz_attributes_()
-#ifdef WIZARD
 #define wiz_detect()	   wiz_detect_()
 #define wiz_genesis()	   wiz_genesis_()
 #define wiz_identify()	   wiz_identify_()
@@ -34,7 +33,6 @@
 #define wiz_map()	   wiz_map_()
 #define wiz_where()	   wiz_where_()
 #define wiz_wish()	   wiz_wish_()
-#endif
 
 /* ### display.c ### */
 #define doredraw() doredraw_()

src/allmain.c
@@ -53,10 +53,9 @@ moveloop()
     dragons_init();
     shop_selection_init();
 
-#ifdef WIZARD
-    if (wizard) add_debug_extended_commands();
-#endif
-
+    if (wizard) {
+	add_debug_extended_commands();
+    }
     (void) encumber_msg(); /* in case they auto-picked up something */
 
     u.uz0.dlevel = u.uz.dlevel;
@@ -394,10 +393,8 @@ moveloop()
 	    !(moves % 15) && !rn2(2))
 		do_vicinity_map();
 
-#ifdef WIZARD
 	if (iflags.sanity_check)
 	    sanity_check();
-#endif
 
 #ifdef CLIPPING
 	/* just before rhack */

src/alloc.c
@@ -10,9 +10,7 @@
 #define EXTERN_H	/* comment line for pre-compiled headers */
 #include "config.h"
 
-#if defined(MONITOR_HEAP) || defined(WIZARD)
 char *FDECL(fmt_ptr, (const genericptr,char *));
-#endif
 
 #ifdef MONITOR_HEAP
 #undef alloc
@@ -42,8 +40,6 @@ unsigned int lth;
 }
 
 
-#if defined(MONITOR_HEAP) || defined(WIZARD)
-
 # if defined(MICRO) || defined(WIN32)
 /* we actually want to know which systems have an ANSI run-time library
  * to know which support the new %p format for printing pointers.
@@ -72,8 +68,6 @@ char *buf;
 	Sprintf(buf, PTR_FMT, (PTR_TYP)ptr);
 	return buf;
 }
-
-#endif
 
 #ifdef MONITOR_HEAP
 

commit 7bd3b21a2bb2c705be1b1386d6b3d0f42a2ad926

fix ^V broken by 677fb3a3e853c6543a81841bacb8dffce274f843

src/mklev.c
@@ -1823,7 +1823,7 @@ xchar x, y;
 	}
 
 	/* Already set or 2/3 chance of deferring until a later level. */
-	if (source->dnum < n_dgns || (rn2(3))) {
+	if (source->dnum < n_dgns || (rn2(3) && !wizard)) {
 		return;
 	}
 