c0c3779729108bf23ebcad70c22af166c9ba6282
Bulwersator <bulwersator@gmail.com>
2013-05-13 22:01:51 +0200

    allow longer messages

--- include/global.h
+++ include/global.h
@@ -359,9 +359,9 @@ struct version_info {
 #define MAX_SUBROOMS	24	/* max # of subrooms in a given room */
 #define DOORMAX		120	/* max number of doors per level */
 
-#define BUFSZ		256	/* for getlin buffers */
-#define QBUFSZ		128	/* for building question text */
-#define TBUFSZ		300	/* toplines[] buffer max msg: 3 81char names */
+#define BUFSZ		1024	/* for getlin buffers */
+#define QBUFSZ		1024	/* for building question text */
+#define TBUFSZ		1024	/* toplines[] buffer max msg: 3 81char names */
 				/* plus longest prefix plus a few extra words */
 
 #define PL_NSIZ		32	/* name of player, ghost, shopkeeper */
