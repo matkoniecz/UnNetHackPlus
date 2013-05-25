8bc118cf8cf0d4f3d1dbd02a3896fe9c2587e118
Bulwersator <bulwersator@gmail.com>
2013-05-25 11:35:36 +0200

    fixes 2f4831d21c1dce9ff00e17b2d73911fcd3e60737
    Escaping list of vanquished creatures during hallucination was not done properly

--- src/end.c
+++ src/end.c
@@ -1229,7 +1229,7 @@ boolean ask;
 		}
 	    dump_list_end();
 	    if (Hallucination)
-	     putstr(klwin, 0, "and a partridge in a pear tree");
+	     if (c == 'y') putstr(klwin, 0, "and a partridge in a pear tree");
 	    if (ntypes > 1) {
 		if (c == 'y') putstr(klwin, 0, "");
 		Sprintf(buf, "%ld creatures vanquished.", total_killed);
